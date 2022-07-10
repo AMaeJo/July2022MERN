d97661fa76083f6eeb010ff6167173230e91790c
Abigail Johnson <90955235+AMaeJo@users.noreply.github.com>
2022-07-09 22:12:56 -0400

    Create App.js

---
+++ Assignments/first_react_jsx/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h3>Things I need to do:</h3>
+      <ul>
+        <li>Learn Respect</li>
+        <li>Discover where I can find the audacity</li>
+        <li>Run super fast</li>
+        <li>Adopt more dogs</li>
+        <li>Destory mankind</li>
+        <li>Become Queen</li>
+      </ul>
+    </div>
+  );
+
+}
+
+
+export default App;
