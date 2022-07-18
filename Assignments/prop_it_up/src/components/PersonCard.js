a38a6a530892d538a62db1ab27a42f713f69e208
Abigail Johnson <90955235+AMaeJo@users.noreply.github.com>
2022-07-17 21:31:16 -0400

    Update PersonCard.js

--- Assignments/prop_it_up/src/components/PersonCard.js
+++ Assignments/prop_it_up/src/components/PersonCard.js
@@ -1,15 +1,13 @@
-import React, { useState } from "react";
 
 const PersonCard = (props) => {
     const {firstname, lastname, age, zodiacsign} = props;
-    const [stateAge, setStateAge] = useState(age);
 
     return (
         <div>
             <h2>
                 {lastname}, {firstname}
             </h2>
-            <p/>Age: {stateAge}
+            <p/>Age: {age}
             <p/>Zodiac Sign: {zodiacsign}
         </div>
     );
